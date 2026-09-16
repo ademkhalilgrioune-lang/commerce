42c01fc3f457e4f9c92d63fa715952c43a211c01
ademkhalilgrioune-lang <ademkhalilgrioune@gmail.com>
2026-09-16 18:17:58 +0100

    Update axiosConfig.js

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,31 +1,23 @@
 import axios from 'axios';
 
-// ==========================================
-// 1. CRÉER UNE INSTANCE AXIOS
-// ==========================================
 const api = axios.create({
-    baseURL: 'http://192.168.100.6:5001/api',  // ← URL du backend
-    /*    baseURL: 'http://192.168.100.6:5001/api',  // ← URL du backend*/
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
-// ==========================================
-// 2. INTERCEPTEUR : AJOUTER LE TOKEN AUTOMATIQUEMENT
-// ==========================================
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('token');
+
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
+
         return config;
     },
     (error) => Promise.reject(error)
 );
 
-// ==========================================
-// 3. EXPORTER L'INSTANCE
-// ==========================================
 export default api;
