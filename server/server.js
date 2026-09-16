2416643ca350fefd8cda350834eccba75f3bc5f0
ademkhalilgrioune-lang <ademkhalilgrioune@gmail.com>
2026-09-16 18:48:54 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ require('dotenv').config();
 // ==========================================
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+app.set('trust proxy', 1);
 // ==========================================
 // 3. MIDDLEWARE
 // ==========================================
